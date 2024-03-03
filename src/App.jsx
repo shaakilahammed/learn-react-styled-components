1f50c3dc3f036944fab7286054528a48536fd47c
shaakilahammed <shaakilahammed@gmail.com>
2024-03-04 00:40:02 +0600

    feat: Card using styled components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// import "./assets/css/dark.css";
+import Tutorial from './components/Tutorial';
+import GlobalStyle from './components/styles/GlobalStyle.styles';
+
+export default function App() {
+    return (
+        <>
+            <GlobalStyle />
+            <Tutorial />
+        </>
+    );
+}
